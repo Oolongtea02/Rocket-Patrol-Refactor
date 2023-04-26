155c0e266a941b4300ae10e553ef0031c7a084c1
Samuel <sashzhan@ucsc.edu>
2023-04-26 16:54:20 -0700

    Updated emitter

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,6 +10,9 @@ class Play extends Phaser.Scene {
         this.load.image('starfield', './assets/starfield.png');
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        //load flare particles
+        this.load.atlas('flares', 'assets/particles/flares.png', 'assets/particles/flares.json');
+
     }
 
     create() {
@@ -38,6 +41,13 @@ class Play extends Phaser.Scene {
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
+        //var input;
+        //input = this.input;
+        //text = this.add.text('100', '100', 'test');
+        //text.setInteractive;
+        //text.on('pointerup', function () {})
+        //this.input.on('gameobjectdown', () => {})
+
         // animation config
         this.anims.create({
             key: 'explode',
@@ -48,6 +58,25 @@ class Play extends Phaser.Scene {
             }),
             frameRate: 30
         });
+
+        const emitter = this.add.particles(400, 250, 'flares', {
+            frame: [ 'red', 'yellow', 'green' ],
+            lifespan: 4000,
+            speed: { min: 150, max: 250 },
+            scale: { start: 0.8, end: 0 },
+            gravityY: 150,
+            blendMode: 'ADD',
+            emitting: false
+        });
+
+        this.input.on('pointerdown', pointer => {
+
+            emitter.explode(16);
+
+        });
+
+        this.add.text(10, 10, 'Click to explode emit particles');
+    
 
         // initialize score
         this.p1Score = 0;
@@ -143,3 +172,15 @@ class Play extends Phaser.Scene {
         this.sound.play('sfx_explosion');
       }
 }
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000',
+    parent: 'phaser-play',
+    scene: Play
+};
+    
+const game = new Phaser.Game(config);
+
