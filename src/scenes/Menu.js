3f39386de1d5fecfaac641169359fa38973f8743
Samuel <sashzhan@ucsc.edu>
2023-05-02 01:15:05 -0700

    Loaded explosion audios

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,6 +8,11 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        //load 4 explosion sound effects
+        this.load.audio('sfx_explosion0', './assets/explosion1.wav');
+        this.load.audio('sfx_explosion1', './assets/explosion2.wav');
+        this.load.audio('sfx_explosion2', './assets/explosion3.wav');
+        this.load.audio('sfx_explosion3', './assets/explosion4.wav')
         //load background soundtrack
         this.load.audio('music_sushiGo', './assets/sushiGo.mp3');
     }
